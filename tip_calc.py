5d85ae3a6abd1ba00a4a7e8641a6f64ef35bd5d0
Kim Long <kimrlong09@gmail.com>
2020-11-11 17:07:17 -0500

    Tip_calc2

--- tip_calc.py
+++ tip_calc.py
@@ -1,8 +1,3 @@
-bill = 50.00
-#service was good
-tip_amount = .20
 
-total_bill = (bill * tip_amount)
-print(total_bill)
 
 
---
+++ tip_calc2.py
@@ -0,0 +1,21 @@
+bill = int(input("what is the bill price?"))
+service = input("How was your service? good? fair? bad?").lower()
+split = int(input("Split how many ways"))
+if(service == "good"):
+    total_bill = bill+bill*0.2
+    print(bill * 0.20)
+    print("your tip amount was " + str(bill*0.2))
+    print("Your total amount is " + str(total_bill))
+    print("each person pays " + str(total_bill/split))
+elif(service == "fair"):
+    total_bill = bill+bill*1.15
+    print(bill * 0.15)
+    print("your tip amount was " + str(bill*0.15))
+    print("Your total amount is " + str(total_bill))
+    print("each person pays " + str(total_bill/split))
+elif(service == "bad"):
+    total_bill = bill+bill*0.10
+    print(bill * 0.10)
+    print("your tip amount was " + str(bill*0.10))
+    print("Your total amount is " + str(total_bill))
+    print("each person pays " + str(total_bill/split))
