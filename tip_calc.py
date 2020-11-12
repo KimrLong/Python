440d89d0171b1f2ea1837a3688a19e3dca6a856b
Kim Long <kimrlong09@gmail.com>
2020-11-11 20:22:30 -0500

    fixed calc

--- tip_calc.py
+++ tip_calc.py
@@ -1,3 +1,16 @@
+bill = int(input("what is the bill price? : "))
+service = input("How was your service? good? fair? bad? : ").lower()
+if(service == "good"):
+    total_bill = bill+bill*0.20
+    print(bill * 0.20)
+    print("your tip amount is: " + str(bill*0.20))
+elif(service == "fair"):
+    total_bill = bill+bill*0.15
+    print(bill * 0.15)
+    print("your tip amount is: " + str(bill*0.15))
+elif(service == "bad"):
+    total_bill = bill+bill*0.10
+    print(bill * 0.10)
+    print("your tip amount is: " + str(bill*0.10))
 
-
-
+print("Your total amount is: " + str(total_bill))
