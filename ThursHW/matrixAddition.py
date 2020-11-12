d46bc1c0d1ee5be7bd24ba7cbc623f0470761551
Kim Long <kimrlong09@gmail.com>
2020-11-12 16:58:22 -0500

    matrixAddition solution

---
+++ ThursHW/matrixAddition.py
@@ -0,0 +1,14 @@
+
+
+x = [[ 1, 2], [3, 4]]
+
+y = [[3, 2], [1, 0]]
+
+result = [[0, 0], [0, 0]]
+
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        result[i][j] = x[i][j] + y[i][j]
+
+for r in result:
+    print(r)
