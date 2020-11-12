81de8a9491a4205f496f9d8c268418172de5714a
Kim Long <kimrlong09@gmail.com>
2020-11-11 23:02:57 -0500

    needs editing

---
+++ coins.py
@@ -0,0 +1,14 @@
+#Coins you want
+
+response = "yes"
+while response = "yes":
+    coins = 0
+    print("You have %d coins. " % coins)
+    response = print("Do you want another?")
+    if response = "no"
+        print("Bye")
+    else: 
+        coins + 1
+
+
+
