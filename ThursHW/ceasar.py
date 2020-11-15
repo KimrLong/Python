5a8c34900bb717c1af181eeee75fc340d38d9d0f
Kim Long <kimrlong09@gmail.com>
2020-11-14 23:53:06 -0500

    review and edit

---
+++ ThursHW/ceasar.py
@@ -0,0 +1,12 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+input_string = "lbh zhfg hayrnea jung lbh unir yrnearq"
+output_string = ""
+for char in input_string:
+    if char != " ":
+        index = alphabet.index(char)
+        new_index = (index + 13) % 26
+        new_char = alphabet[new_index]
+    else:
+        new_char = " "
+    output_string += new_char
+print(output_string)
